55ab2234e10c1e9f0b7c09acd9d51087c7792e58
natkehoe <nhk.jes@gmail.com>
2025-02-28 12:01:58 +1100

    Copy of ROS2-localisation_aruco_marker
    - edited from ROS1 to ROS2 by Clayder Gonzalez
    TODO:
    - debug localisation_aruco_marker.py
    - test on ROS2

---
+++ launch/localisation_aruco_marker.py
@@ -0,0 +1,76 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='tf2_ros',
+            #namespace='turtlesim1',
+            executable='static_transform_publisher',
+            name='optitrack2checkerboard_marker_tf_broadcaster',
+            arguments=[
+                '-0.02037813700735569',
+                '0.023949000984430313',
+                '0.4495094120502472',
+                '-0.0013561244122684002',
+                '0.0009626050014048815',
+                '-0.00037052115658298135',
+                '-0.9999986290931702',
+                'world',       # frame_id
+                'checkerboard_marker'  # child_frame_id
+            ]
+        ),
+        
+        Node(
+            package='tf2_ros',
+            #namespace='turtlesim1',
+            executable='static_transform_publisher',
+            name='checkerboard_marker2checkerboard_tf_broadcaster',
+            arguments=[
+                '0',
+                '-0.014',
+                '0',
+                '0',
+                '-1.57079633',
+                '1.57079633',
+                'checkerboard_marker',       # frame_id
+                'checkerboard'  # child_frame_id
+            ]
+        ),
+        
+        Node(
+            package='tf2_ros',
+            #namespace='turtlesim1',
+            executable='static_transform_publisher',
+            name='checkerboard2camera_tf_broadcaster',
+            arguments=[
+                '0.85672',
+                '0.73191',
+                '-0.74595',
+                '0.4633',
+                '-0.2706',
+                '-0.3857',
+                '0.7506',
+                'checkerboard',
+                'camera'
+            ]
+        ),
+        
+        Node(
+            package='tf2_ros',
+            #namespace='turtlesim1',
+            executable='static_transform_publisher',
+            name='robot_tf_broadcaster',
+            arguments=[
+                '-0.194'
+                '0'
+                '0.01'
+                '0'
+                '0'
+                '0'
+                '1'
+                'marker_frame'
+                'base_link'
+            ]
+        )
+    ])
---
+++ src/localisation_aruco_marker.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world localisation_aruco_marker package\n");
+  return 0;
+}
